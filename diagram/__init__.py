6dd49adb8942c194ec1f1b0e08abf71270d44852
Stuart Sears <stuart@sjsears.com>
2019-10-24 12:21:32 +0100

    move to safe_load for modern PyYAML

--- diagram/__init__.py
+++ diagram/__init__.py
@@ -5,7 +5,7 @@ __version__ = '1.0.0'
 __author__ = 'Derek Payton <derek.payton@gmail.com>'
 __license__ = 'MIT'
 
-config = yaml.load(pkg_resources.resource_string(__name__, 'config.yml'))
+config = yaml.safe_load(pkg_resources.resource_string(__name__, 'config.yml'))
 CHORD_STYLE = config['chord']
 FRETBOARD_STYLE = config['fretboard']
 
