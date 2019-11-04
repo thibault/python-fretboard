b70220c87810d6dbcfdbc8527aa39c3c55f867b7
Stuart Sears <stuart@sjsears.com>
2019-11-04 15:54:46 +0000

    adds MultiFingerChord class

--- diagram/__init__.py
+++ diagram/__init__.py
@@ -9,5 +9,5 @@ config = yaml.safe_load(pkg_resources.resource_string(__name__, 'config.yml'))
 CHORD_STYLE = config['chord']
 FRETBOARD_STYLE = config['fretboard']
 
-from .chord import GuitarChord, BassChord, UkuleleChord
+from .chord import GuitarChord, BassChord, UkuleleChord, MultiFingerChord
 from .fretboard import GuitarFretboard, BassFretboard, UkuleleFretboard
